f892913d17b66525cdadd464ea35d2a0003fc547
credefl <heechan1208@gmail.com>
2025-04-07 14:31:57 +0900

    del views

--- src/views/History.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div>
-    <TransactionHistory></TransactionHistory>
-  </div>
-</template>
-
-<script setup>
-import TransactionHistory from '@/components/TransactionHistory/TransactionHistory.vue';
-</script>
